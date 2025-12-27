08ca9a5aad1fa34b1c0dc935683e6d0fd4823a5b
adex720 <25402611+adex720@users.noreply.github.com>
2025-12-27 12:47:08 +0200

    Add loot tables, harvesting tools and mining levels

--- src/client/java/com/adex/datagen/ModDataGenerator.java
+++ src/client/java/com/adex/datagen/ModDataGenerator.java
@@ -3,13 +3,14 @@ package com.adex.datagen;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
-public class DataGenerator implements DataGeneratorEntrypoint {
+public class ModDataGenerator implements DataGeneratorEntrypoint {
 
     @Override
     public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
         FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
         pack.addProvider(ModModelProvider::new);
+        pack.addProvider(ModLootTableGenerator::new);
     }
 
 }
---
+++ src/client/java/com/adex/datagen/ModLootTableGenerator.java
@@ -0,0 +1,64 @@
+package com.adex.datagen;
+
+import com.adex.block.ModBlocks;
+import com.adex.item.ModItems;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.storage.loot.entries.LootItem;
+import net.minecraft.world.level.storage.loot.functions.ApplyBonusCount;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModLootTableGenerator extends FabricBlockLootTableProvider {
+
+    public ModLootTableGenerator(FabricDataOutput output, CompletableFuture<HolderLookup.Provider> lookup) {
+        super(output, lookup);
+    }
+
+    @Override
+    public void generate() {
+        dropSelf(ModBlocks.HARDENED_STONE);
+
+        dropSelf(ModBlocks.GABBRO);
+        dropSelf(ModBlocks.LARVIKITE);
+        dropSelf(ModBlocks.SERPENTINITE);
+        dropSelf(ModBlocks.SLATE);
+        dropSelf(ModBlocks.TRAVERTINE);
+
+        dropSelf(ModBlocks.CHALCEDONY_BLOCK);
+        dropSelf(ModBlocks.GARNET_BLOCK);
+        dropSelf(ModBlocks.JADE_BLOCK);
+        dropSelf(ModBlocks.JASPER_BLOCK);
+        dropSelf(ModBlocks.ONYX_BLOCK);
+        dropSelf(ModBlocks.OPAL_BLOCK);
+        dropSelf(ModBlocks.RUBY_BLOCK);
+        dropSelf(ModBlocks.SAPPHIRE_BLOCK);
+        dropSelf(ModBlocks.SPINEL_BLOCK);
+        dropSelf(ModBlocks.TIGERS_EYE_BLOCK);
+
+        dropOre(ModBlocks.CHALCEDONY_ORE, ModItems.CHALCEDONY);
+        dropOre(ModBlocks.GARNET_ORE, ModItems.GARNET);
+        dropOre(ModBlocks.JADE_ORE, ModItems.JADE);
+        dropOre(ModBlocks.JASPER_ORE, ModItems.JASPER);
+        dropOre(ModBlocks.ONYX_ORE, ModItems.ONYX);
+        dropOre(ModBlocks.OPAL_ORE, ModItems.OPAL);
+        dropOre(ModBlocks.RUBY_ORE, ModItems.RUBY);
+        dropOre(ModBlocks.SAPPHIRE_ORE, ModItems.SAPPHIRE);
+        dropOre(ModBlocks.SPINEL_ORE, ModItems.SPINEL);
+        dropOre(ModBlocks.TIGERS_EYE_ORE, ModItems.TIGERS_EYE);
+
+        dropSelf(ModBlocks.REINFORCED_ANCIENT_DEBRIS);
+    }
+
+    public void dropOre(Block block, Item item) {
+        HolderLookup.RegistryLookup<Enchantment> registryLookup = registries.lookupOrThrow(Registries.ENCHANTMENT);
+        add(block, createSilkTouchDispatchTable(block, applyExplosionDecay(block, LootItem.lootTableItem(item)
+                .apply(ApplyBonusCount.addOreBonusCount(registryLookup.getOrThrow(Enchantments.FORTUNE))))));
+    }
+}
