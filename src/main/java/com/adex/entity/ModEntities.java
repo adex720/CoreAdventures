7ee56971a56a4ea862b0bc9df83275aad3b75fba
adex720 <25402611+adex720@users.noreply.github.com>
2026-01-11 08:47:53 +0200

    Clean code

--- src/main/java/com/adex/entity/ModEntities.java
+++ src/main/java/com/adex/entity/ModEntities.java
@@ -3,14 +3,12 @@ package com.adex.entity;
 import com.adex.CoreAdventures;
 import com.adex.entity.golem.*;
 import com.adex.entity.projectile.GolemFireBall;
-import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.EntityDimensions;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
 
