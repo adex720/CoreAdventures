c695532740b095a1bdc23864fffb3184fbcddda2
adex720 <25402611+adex720@users.noreply.github.com>
2026-01-11 11:43:22 +0200

    Make spinel golem invulnerable to arrow damage

--- src/main/java/com/adex/entity/golem/SpinelGolem.java
+++ src/main/java/com/adex/entity/golem/SpinelGolem.java
@@ -1,11 +1,19 @@
 package com.adex.entity.golem;
 
 import com.adex.entity.ai.ArrowAttackGoal;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.BossEvent;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.damagesource.DamageType;
+import net.minecraft.world.damagesource.DamageTypes;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.ai.goal.Goal;
 import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.level.Level;
+import org.jspecify.annotations.NonNull;
+
+import java.util.Optional;
 
 public class SpinelGolem extends Golem {
 
@@ -16,5 +24,12 @@ public class SpinelGolem extends Golem {
     @Override
     public Goal getSpecialGoal() {
         return new ArrowAttackGoal(this, getMeleeRange(), getRangedRange(), 10, 6.0f, 3.0f);
+    }
+
+    @Override
+    public boolean isInvulnerableTo(@NonNull ServerLevel serverLevel, @NonNull DamageSource damageSource) {
+        Optional<ResourceKey<DamageType>> damageType = damageSource.typeHolder().unwrapKey();
+        if (damageType.isPresent() && damageType.get() == DamageTypes.ARROW) return true;
+        return super.isInvulnerableTo(serverLevel, damageSource);
     }
 }
