6f0da878f9f7dbeeabe799f660b015a797f8655a
adex720 <25402611+adex720@users.noreply.github.com>
2026-01-28 11:22:52 +0200

    Fix sentry texture

--- src/client/java/com/adex/entity/sentry/SentryModel.java
+++ src/client/java/com/adex/entity/sentry/SentryModel.java
@@ -31,10 +31,10 @@ public class SentryModel extends EntityModel<SentryRenderState> {
 
         partdefinition.addOrReplaceChild("body", CubeListBuilder.create().texOffs(0, 16).addBox(-4.0f, -6.0f, -2.0f, 8.0f, 12.0f, 4.0f, new CubeDeformation(0.0f)), PartPose.offset(0.0f, 6.0f, 0.0f));
         partdefinition.addOrReplaceChild("head", CubeListBuilder.create().texOffs(0, 0).addBox(-4.0f, -6.0f, -4.0f, 8.0f, 8.0f, 8.0f, new CubeDeformation(0.0f)), PartPose.offset(0.0f, -2.0f, 0.0f));
-        partdefinition.addOrReplaceChild("handRight", CubeListBuilder.create().texOffs(16, 32).addBox(0.0f, -2.0f, -2.0f, 4.0f, 12.0f, 4.0f, new CubeDeformation(0.0f)), PartPose.offset(4.0f, 2.0f, 0.0f));
-        partdefinition.addOrReplaceChild("handLeft", CubeListBuilder.create().texOffs(32, 0).addBox(-4.0f, -2.0f, -2.0f, 4.0f, 12.0f, 4.0f, new CubeDeformation(0.0f)), PartPose.offset(-4.0f, 2.0f, 0.0f));
-        partdefinition.addOrReplaceChild("legRight", CubeListBuilder.create().texOffs(24, 16).addBox(-2.0f, -0.0f, -2.0f, 4.0f, 12.0f, 4.0f, new CubeDeformation(0.0f)), PartPose.offset(2.0f, 12.0f, 0.0f));
-        partdefinition.addOrReplaceChild("legLeft", CubeListBuilder.create().texOffs(0, 32).addBox(-2.0f, -0.0f, -2.0f, 4.0f, 12.0f, 4.0f, new CubeDeformation(0.0f)), PartPose.offset(-2.0f, 12.0f, 0.0f));
+        partdefinition.addOrReplaceChild("handRight", CubeListBuilder.create().texOffs(32, 0).addBox(0.0f, -2.0f, -2.0f, 4.0f, 12.0f, 4.0f, new CubeDeformation(0.0f)), PartPose.offset(4.0f, 2.0f, 0.0f));
+        partdefinition.addOrReplaceChild("handLeft", CubeListBuilder.create().texOffs(16, 32).addBox(-4.0f, -2.0f, -2.0f, 4.0f, 12.0f, 4.0f, new CubeDeformation(0.0f)), PartPose.offset(-4.0f, 2.0f, 0.0f));
+        partdefinition.addOrReplaceChild("legRight", CubeListBuilder.create().texOffs(0, 32).addBox(-2.0f, -0.0f, -2.0f, 4.0f, 12.0f, 4.0f, new CubeDeformation(0.0f)), PartPose.offset(2.0f, 12.0f, 0.0f));
+        partdefinition.addOrReplaceChild("legLeft", CubeListBuilder.create().texOffs(24, 16).addBox(-2.0f, -0.0f, -2.0f, 4.0f, 12.0f, 4.0f, new CubeDeformation(0.0f)), PartPose.offset(-2.0f, 12.0f, 0.0f));
         return LayerDefinition.create(meshdefinition, 64, 64);
     }
 
