fee74e49d08a4619654362d52fd3a89cf347a986
adex720 <25402611+adex720@users.noreply.github.com>
2026-01-30 17:45:25 +0200

    Spawn sentries in refuge

--- src/main/java/com/adex/entity/sentry/Sentry.java
+++ src/main/java/com/adex/entity/sentry/Sentry.java
@@ -11,7 +11,6 @@ import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraft.world.entity.ai.behavior.BehaviorUtils;
 import net.minecraft.world.entity.ai.goal.*;
 import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
 import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
