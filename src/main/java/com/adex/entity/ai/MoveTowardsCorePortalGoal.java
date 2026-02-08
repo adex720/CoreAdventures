8387fa127c07975db019b1651f5d6d495dfa3c4c
adex720 <25402611+adex720@users.noreply.github.com>
2026-02-08 16:19:42 +0200

    Make MoveTowardsCorePortalGoal pathfinding golems inside portal instead of right next to one

--- src/main/java/com/adex/entity/ai/MoveTowardsCorePortalGoal.java
+++ src/main/java/com/adex/entity/ai/MoveTowardsCorePortalGoal.java
@@ -59,7 +59,7 @@ public class MoveTowardsCorePortalGoal extends Goal {
     @Override
     public void start() {
         IS_GOLEM_PATHFINDING = true;
-        golem.getNavigation().moveTo(wantedX, wantedY, wantedZ, 1, speedModifier);
+        golem.getNavigation().moveTo(wantedX, wantedY, wantedZ, 0, speedModifier);
         IS_GOLEM_PATHFINDING = false;
     }
 
